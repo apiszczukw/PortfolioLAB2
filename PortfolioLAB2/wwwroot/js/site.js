5d928c6ac2ba6167228120e9834acdd9b1f3aa38
apiszczukw <apiszczukw@gmail.com>
2025-04-15 16:27:06 +0200

    Menu na wąskie ekrany (CSS)

--- PortfolioLAB2/wwwroot/js/site.js
+++ PortfolioLAB2/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+function ShowMenu() {
+    const linki = document.querySelector(".nav-links");
+    linki.classList.toggle("show-links");
+}
